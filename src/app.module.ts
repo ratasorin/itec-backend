cfc444a39e7c3ac5e3a36cce9589dee8cc1ccdba
ratasorin <ratasorin0@gmail.com>
2022-05-28 13:30:15 +0300

    [Fix] Add every missing module dependency

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
 import { UserService } from './user/user.service';
 import { UserModule } from './user/user.module';
 import { BookingService } from './booking/booking.service';
+import { BookingModule } from './booking/booking.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, UserModule, UserModule, BookingModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService, UserService, BookingService],
 })
 export class AppModule {}
